661ab5e45162e0982aea356c1f3640a355013fc3
M4rioYT <82200759+Pal404error@users.noreply.github.com>
2021-04-17 12:22:59 +0530

    Update example.java

--- example.java
+++ example.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class example
 {
 	public static void main(String []args)
 	{
-		int num = 56;
+		Random ran = new Random();
+		int num = ran.nextInt(100);
 		int j = 5;
 		Scanner s = new Scanner(System.in);
 		System.out.println("\n \t ????????????? GUESS THE NUMBER ??????????");
+		System.out.println("\n \t GENERATING RANDOM NUMBER ................");
 		System.out.println("\n \t START ENTERING NUMBERS TO GUESS RIGHT ONE ");
 		for (int i = 0;i<5;i++)
 		{
@@ -24,6 +28,6 @@ public class example
 			{
 				System.out.println("\n \t HOORAY YOU GUESSED THE NUMBER ");
 			}
-		}
+		} 
 	}
 }
